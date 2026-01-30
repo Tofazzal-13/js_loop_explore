2ccd027b7ec86632f6b1a16e507da22874a27465
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-01-30 12:01:52 +0600

    even number and range using while loop

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 17;
+if(number<10){
+    console.log("choto ekta number");
+    
+}
+console.log("something new is cooking");
+
+// while loop 
+let num = 0; 
+while(num < 5){
+    console.log("looooping _",num);
+    num++; 
+}
