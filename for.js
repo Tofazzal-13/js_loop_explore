cca349c318401354471fa95d5372e0b59d7496ef
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-01-31 12:36:37 +0600

    multiplication table using for loop

---
+++ for.js
@@ -0,0 +1,27 @@
+for(let num=0; num<=10; num++){
+    // console.log(num);
+    
+}
+
+// odd number print using for loop
+
+for(let i = 1; i <= 20; i+=2){
+    // console.log("print odd number : ", i);
+    
+}
+
+// even number print using for loop
+for(let i = 0; i <= 20; i+=2){
+    // console.log("print even number : ", i);
+    
+}
+
+
+// sum of number 11 to 20 
+
+let sum = 0;
+
+for(let i = 11; i <= 20; i++){
+    sum += i;
+}
+console.log("total sum : " , sum);
