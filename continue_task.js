46ece4051621bc0a81423182e17115821db08ac9
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-01 23:49:44 +0600

    continue_task.js

--- continue_task.js
+++ continue_task.js
@@ -4,5 +4,37 @@ while(num < 40){
     if(num % 2 === 1){
         continue;
     }
-    console.log("Display 1 to 40 even Numbers:", num);
+    // console.log("Display 1 to 40 even Numbers:", num);
+}
+
+
+/* let i = 55;
+
+while(i < 85){
+    i += 2;
+    if(i % 5 === 0 && i % 2 === 1){
+        continue
+    }
+    console.log(i);
+    
+} */
+
+
+let num1 = 55;
+
+while (num1 <= 85) {
+  // skip even numbers
+  if (num1 % 2 === 0) {
+    num1++;
+    continue;
+  }
+
+  // skip numbers divisible by 5
+  if (num1 % 5 === 0) {
+    num1++;
+    continue;
+  }
+
+  console.log(num1);
+  num1++;
 }
